ac37f4db7d77e256e273dd72458f812c12c10ce6
Martin <martin.khannouz@gmail.com>
2019-01-07 17:59:43 -0400

    Update skeleton.

--- answers/answer_skeleton.py
+++ answers/answer_skeleton.py
@@ -1,13 +1,13 @@
 import os
-from pyspark.sql import DataFrame
+import sys
 from pyspark.rdd import RDD
-from pyspark.sql.functions import desc
+from pyspark.sql import Row
+from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import desc
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.sql import Row
-from pyspark.sql.functions import lit
-import sys
 
 spark = SparkSession \
     .builder \
@@ -27,17 +27,79 @@ def toCSVLine(data):
     return None
 
 def basic_als_recommender(filename, seed):
+    '''
+    Write a code that prints the RMSE of recommendations obtained
+    through ALS collaborative filtering, similarly to the example at
+    http://spark.apache.org/docs/latest/ml-collaborative-filtering.html
+    The training ratio must be 80% and the test ratio must be 20%. The
+    random seed used to sample the training and test sets (passed to
+    `DataFrame.randomSplit`) must be an argument of the script. The seed
+    must also be used to initialize the ALS optimizer (use
+    *ALS.setSeed()*). The following parameters must be used in the ALS
+    optimizer:
+    - maxIter: 5
+    - rank: 70
+    - regParam: 0.01
+    - coldStartStrategy: 'drop'
+    Test file: tests/test_basic_als.py
+    '''
     return 0
 
 def global_average(filename, seed):
+    '''
+    Write a code that prints the global average rating for all users and
+    all movies in the training set. Training and test
+    sets should be determined as before (e.g: as in function basic_als_recommender).
+    Test file: tests/test_global_average.py
+    '''
     return 0
 
 def global_average_recommender(filename, seed):
+    '''
+    Write a code that prints the RMSE of recommendations obtained
+    through global average, that is, the predicted rating for each
+    user-movie pair must be the global average computed in the previous
+    task. Training and test
+    sets should be determined as before. You can add a column to an existing DataFrame with function *.withColumn(...)*.
+    Test file: tests/test_global_average_recommender.py
+    '''
     return 0
 
 def means_and_interaction(filename, seed, n):
-    return " bb"
+    '''
+    Write a code that prints the *n* first elements of a DataFrame
+    containing, for each (userId, movieId, rating) triple, the
+    corresponding user mean (computed on the training set), item mean
+    (computed on the training set) and user-item interaction *i* defined
+    as *i=rating-(user_mean+item_mean-global_mean)*. *n* must be passed on
+    the command line. The DataFrame must contain the following columns:
+
+    - userId # as in the input file
+    - movieId #  as in the input file
+    - rating # as in the input file
+    - user_mean # computed on the training set
+    - item_mean # computed on the training set
+    - user_item_interaction # i = rating - (user_mean+item_mean-global_mean)
+
+    Rows must be ordered by ascending userId and then by ascending movieId.
+
+    Training and test sets should be determined as before.
+    Test file: tests/test_means_and_interaction.py
+
+    Note, this function should return a list of collected Rows. Please, have a
+    look at the test file to ensure you have the right format.
+    '''
+    return []
 
 def als_with_bias_recommender(filename, seed):
+    '''
+    Write a code that prints the RMSE of recommendations obtained by predicting
+    the user-item interaction *i* using ALS. Your ALS model should make predictions
+    for *i*, then you should recompute the predicted rating with the formula
+    *i+user_mean+item_mean-m* (*m* is the global rating) The RMSE should compare
+    the original rating column and the predicted rating column.  Training and test
+    sets should be determined as before. Your ALS model should use the same
+    parameters as before and be initialized with the random seed passed as parameter.
+    Test file: tests/test_als_with_bias_recommender.py
+    '''
     return 0
-
