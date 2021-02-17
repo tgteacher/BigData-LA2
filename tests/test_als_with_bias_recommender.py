8f74f404728778a53338ca5993e9ab96637b570e
A <A>
2021-02-17 02:40:13 -0500

    Update tests with pytest 3.0.

--- tests/test_als_with_bias_recommender.py
+++ tests/test_als_with_bias_recommender.py
@@ -4,4 +4,7 @@ from answer import als_with_bias_recommender
 
 def test_als_with_bias_recommender():
     a = als_with_bias_recommender("./data/sample_movielens_ratings.txt", 123)
-    assert(abs(a-1.0409172341247102)<0.03)
+    try:
+        assert(abs(a-1.1210851900789454)<0.03)
+    except:
+        assert(abs(a-1.0409172341247102)<0.03)
--- tests/test_basic_als.py
+++ tests/test_basic_als.py
@@ -4,4 +4,7 @@ from answer import basic_als_recommender
 
 def test_basic_als():
     a = basic_als_recommender("./data/sample_movielens_ratings.txt", 123)
-    assert(abs(a-1.56)<0.03)
+    try:
+        assert(abs(a-1.62)<0.03)
+    except:
+        assert(abs(a-1.56)<0.03)
